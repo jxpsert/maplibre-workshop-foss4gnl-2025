6e0523bd981c308e93ff351a241c14e56e40b670
Jasper Platenburg <65042215+jxpsert@users.noreply.github.com>
2025-07-02 09:47:00 +0000

    starting location

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 import * as maplibregl from "https://esm.sh/maplibre-gl";
+import { Protocol } from "https://esm.sh/pmtiles";
+const protocol = new Protocol();
+maplibregl.addProtocol("pmtiles", protocol.tile);
 
 const map = new maplibregl.Map({
     container: 'mijnkaart',
-    style: 'https://tiles.openfreemap.org/styles/liberty',
-    center: [5.04781, 51.64956],
+    style: './assets/style.json',
+    center: [5.66457, 51.96704],
     zoom: 15
 });
